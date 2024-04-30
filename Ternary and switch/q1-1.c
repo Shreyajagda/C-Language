58755a86363e14cdb71a2d7017f71ee93bad0dc4
Shreyajagda <167760364+Shreyajagda@users.noreply.github.com>
2024-04-30 15:25:20 +0530

    Add files via upload

---
+++ Ternary and switch/q1-1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+main (){
+    int a;
+    int b;
+    int c;
+
+    printf("Enter your first number :");
+    scanf("%d",&a);
+     printf("Enter your second number :");
+    scanf("%d",&b);
+     printf("Enter your third number :");
+    scanf("%d",&c);
+
+    (a<b) ? 
+    (a<c) ? printf("A is Min") : printf("C is Min")
+    :
+    (b<c) ? printf("B is Min") : printf("C is Min");
+
+}
