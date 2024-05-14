cee7a3d6d73e7e4d65479d8cf466bf01556546bc
Shreyajagda <167760364+Shreyajagda@users.noreply.github.com>
2024-05-14 20:44:06 +0530

    Add files via upload

---
+++ 1D Array/q-4.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+    int size;
+    int number = 1;
+    printf("Enter the size of an Array :");
+    scanf("%d",&size);
+    
+   
+    int arr[size] ;
+    for(int index = 0; index < size; index++){
+        printf("Enter the value of a[%d] : ", index);
+        scanf("%d",&arr[index]);
+    }
+
+    for (int index = 0; index < size; index++)
+    {
+        if ( arr[index] % 2!=0)
+        {
+            printf("Odd number %d : %d\n", number++, arr[index]);
+        }
+    }
+}
