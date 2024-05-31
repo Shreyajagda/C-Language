e737ceab89ad7cdfa7335b631115b443388dee2c
Shreyajagda <167760364+Shreyajagda@users.noreply.github.com>
2024-05-31 17:00:02 +0530

    Add files via upload

---
+++ pointer-2/q-3.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int max(int *a, int *b);
+int main(){
+
+    int a, b;
+
+    printf("Enter first numbers: ");
+    scanf("%d", &a);
+
+    printf("Enter second numbers: ");
+    scanf("%d", &b);
+
+    int ans = max(&a, &b);
+
+    printf("Max values: %d", ans);
+
+    return 0;
+}
+
+int max(int *a, int *b){
+    if (*a > *b){
+        return *a;
+    }
+    else{
+        return *b;
+    }
+
+}
+ 
---
+++ pointer-2/q-5.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    char var = 'x';
+
+    int *ptr1 = &var;
+    int **ptr2 = &ptr1;
+
+    printf("%u\t%u\t%c\n", ptr2, *ptr2, **ptr2);
+    return 0;
+}
